d6aaa05964ac5b34f36d7c9b1dc8a6de8cc1168e
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-02 16:50:09 -0600

    Solve tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return Boolean (2>1)
+    } else if ((year % 4 === 0) && (year % 100 === 0)) {
+        return Boolean (1>2)
+    } else if ((year % 4 === 0) && (year%100 != 0)){
+        return Boolean (2>1)
+    } else {
+        return Boolean (1>2)
+    }
 };
 
 // Do not edit below this line
