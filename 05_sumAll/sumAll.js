2c740a648eed98816c221cc7b9f9e925e51733b7
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-01 18:07:20 -0600

    Solve first four tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,20 @@
 const sumAll = function(num1, num2) {
-    let total = 0
-    if (num1 < 0) {
+    if (((num1 || num2) < 0)) {
         return "ERROR"
     } else {
-        
+        if (num2 > num1) {
+            let total = num1
+            for (i = num1 +1; i <= num2; i++) {
+                total += i
+            } return total
+        } else {
+            let total = num2
+            for (i = num2 +1; i <= num1; i++) {
+                total += i;
+            } return total
+        }
     }
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
