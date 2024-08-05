d0edf325c06eb9997bd6c9ef88d38fff88b132d7
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-05 17:56:39 -0600

    Solve first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let reverseWord = ""
+    for (let i = 1; i <= word.length; i++) {
+        reverseWord += word.at(-i)
+    } if (reverseWord == word) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
