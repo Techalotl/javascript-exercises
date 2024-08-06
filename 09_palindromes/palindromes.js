e03f45220333366958aa9afa91092f033416a12a
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-06 17:50:10 -0600

    Solve tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,21 @@
-const palindromes = function (word) {
-    let reverseWord = ""
-    for (let i = 1; i <= word.length; i++) {
-        reverseWord += word.at(-i)
-    } if (reverseWord == word) {
+const palindromes = function (word, signs = ["!", ",","."," "]) {
+    let reverseWord = []
+    let newWord = []
+    newWord = [...word.toLowerCase()]
+    let onlyLetters = []
+    newWord.forEach((letter => {
+        if (!signs.includes(letter)) {
+            onlyLetters.push(letter);
+        } 
+    }))
+    reverseWord = onlyLetters.toReversed();
+    if (reverseWord.toString() === onlyLetters.toString()) {
         return true;
     } else {
         return false;
     }
-};
+
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
