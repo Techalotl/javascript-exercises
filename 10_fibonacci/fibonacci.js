e879993dbff499fede8818176af6c6934f7f1198
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-07 17:11:06 -0600

    Solve tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS"
+    } else {
+        let sequence = [0,1]
+        for (let i = 2; i <= number; i++) {
+            sequence.push((sequence[i-2]) + (sequence[i-1]));
+        } return sequence[number]
+        
+    }
 };
 
 // Do not edit below this line
