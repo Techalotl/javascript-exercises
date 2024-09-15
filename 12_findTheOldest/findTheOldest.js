010757bfbfa9beac81108279153560fc3cb34dd1
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-09-14 20:06:28 -0600

    Solve the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.map((person) => {if (person.yearOfDeath == undefined) { person.yearOfDeath = (new Date()).getFullYear()}} );
+    array.sort((a,b) => {
+    const firstPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return firstPerson > nextPerson ? -1: 1;
+    }) 
+    return array[0];
 };
 
 // Do not edit below this line
