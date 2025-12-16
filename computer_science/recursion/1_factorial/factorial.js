e47a052469918995ca1f0d71c4baea29ee977539
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-12-16 17:03:23 -0600

    Solves the exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(num) {
+    if (!Number.isInteger(num) || num < 0) {
+        return undefined;
+    } else if (num === 0) {
+        return 1;
+    } else {
+        return num * (factorial(num -1))
+    }
 };
 
 // Do not edit below this line
