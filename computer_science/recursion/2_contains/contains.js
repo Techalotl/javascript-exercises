a30fb4a7232fbc9055b1463ac3887c3cec926a16
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-12-24 14:31:28 -0600

    Solve the exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,12 @@
-const contains = function() {
-  
-};
+const contains = function(obj, value) {
+  if (Object.values(obj).includes(value)) return true;
+  for (let key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      if (contains(obj[key], value)) return true;
+    }
+  }
+  return false
+}; 
   
 // Do not edit below this line
 module.exports = contains;
