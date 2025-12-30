2c2d59faac458e025fa537d8a5801974aef0307d
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2025-12-30 16:02:13 -0600

    Solve the exercise

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,13 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(array) {
+  let sum = 0;
+  if (typeof array !== 'object') return undefined;
+  for (const key in array) {
+    if(typeof array[key] === 'number' && Number.isInteger(array[key])) sum++;
+    if (typeof array[key] === 'object' && array[key] !== null){
+      sum += totalIntegers(array[key])
+    }
+  }
+  return sum
 };
   
 // Do not edit below this line
