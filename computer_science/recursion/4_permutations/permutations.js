a5336fefd59292369b2e8cecd934eec46dc6e651
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2026-01-07 12:51:41 -0600

    Solve the exercise

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,23 @@
-const permutations = function() {
-  
+const permutations = function(array, size = array.length) {
+  const newArray = []
+  if (size === 0 || size === 1) {
+    newArray.push([...array]);
+    return newArray;
+  } 
+  for (let i = 0; i < size; i++) {
+    const recursiveArray = permutations(array, size - 1);
+    newArray.push(...recursiveArray)
+    if (size % 2 === 1) {
+      let temp = array[0]
+      array[0] = array[size - 1];
+      array[size - 1] = temp;
+    } else {
+      let temp = array[i];
+      array[i] = array[size - 1];
+      array[size - 1] = temp;
+    }
+  }
+  return newArray
 };
   
 // Do not edit below this line
